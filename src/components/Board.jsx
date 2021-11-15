2242d24d67d27bbac8e142a3ff5c0eb8b01084c9
baekjaein <jibaek@oss-lab.kr>
2021-11-15 16:12:35 +0900

    Create temp tray func

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -15,6 +15,7 @@ function create2DArray(row, col) {
 
 function Board() {
   const [board, setBoard] = useState(create2DArray(9, 9))
+  const [blockCode, setBlockCode] = useState(0)
 
   const deliverClickIndex = (row, col) => {
     setBlock(row, col)
@@ -22,13 +23,9 @@ function Board() {
 
   const boardRowMap = board.map((row, index) => (<BoardRow value={row} rowIndex={index} deliver={deliverClickIndex} key={index}></BoardRow>))
   
-  const deliverChoiceBlock = () => {
-
-  }
-
   const setBlock = (sRow, sCol) => {
     let tempBoard = board
-    let settedBoard = switchBlocks(tempBoard, 5, sRow, sCol)
+    let settedBoard = switchBlocks(tempBoard, blockCode, sRow, sCol)
     
     if (settedBoard == 0 ) {
       console.log("nothing to do")
@@ -36,8 +33,31 @@ function Board() {
     else if (typeof settedBoard == "object"){
       setBoard([...settedBoard])
     }
-    
-    
+  }
+
+
+
+
+  // temp
+  const passingBlockcode1 = () => {
+    console.log("block setted (1)")
+    setBlockCode(1)
+  }
+  const passingBlockcode2 = () => {
+    console.log("block setted (2)")
+    setBlockCode(2)
+  }
+  const passingBlockcode3 = () => {
+    console.log("block setted (3)")
+    setBlockCode(3)
+  }
+  const passingBlockcode4 = () => {
+    console.log("block setted (4)")
+    setBlockCode(4)
+  }
+  const passingBlockcode5 = () => {
+    console.log("block setted (5)")
+    setBlockCode(5)
   }
 
   return (
@@ -46,7 +66,12 @@ function Board() {
         {boardRowMap}
       </div>
       <div className="tray">
-        gen block here
+        {/* temp */}
+        <div className="tempBtn" onClick={passingBlockcode1}>1 || Horizotal 5 Length bar</div>
+        <div className="tempBtn" onClick={passingBlockcode2}>2 || Horizotal 3 Length bar</div>
+        <div className="tempBtn" onClick={passingBlockcode3}>3 || Vertical 5 Length Bar</div>
+        <div className="tempBtn" onClick={passingBlockcode4}>4 || Vertical 3 Length Bar</div>
+        <div className="tempBtn" onClick={passingBlockcode5}>5 || 2X2 Square</div>
       </div>
     </div>
   );
